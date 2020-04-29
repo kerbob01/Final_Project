a2a45886063403d8be008bf9d316d83494512466
Alan Dalton <58486363+kerbob01@users.noreply.github.com>
2020-04-29 11:12:16 -0600

    adding data

---
+++ election_table_create.sql
@@ -0,0 +1,12 @@
+CREATE table election_data (
+year int,
+state varchar(255),
+state_po varchar(255),
+county varchar(255),
+FIPS varchar(255),
+office varchar(255),
+candidate varchar(255),
+party varchar(255),
+candidatevotes varchar(255),
+totalvotes varchar(255),
+version int);
