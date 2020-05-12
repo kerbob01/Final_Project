a1c1c3284c59a8e649d1ef89355a7e0fc678a803
Alan Dalton <58486363+kerbob01@users.noreply.github.com>
2020-05-12 12:36:13 -0600

    new files and table

---
+++ Tbl_creates and queries/background_checks.sql
@@ -0,0 +1,17 @@
+CREATE table background_checks_by_state (
+Year int,	
+State_Territory varchar(255),
+Jan int,
+Feb int,
+Mar int,
+Apr int,
+May int,
+Jun int,
+Jul int,
+Aug int,
+Sep int,
+Oct int,
+Nov int,
+Dec int,
+Totals int);
+	
